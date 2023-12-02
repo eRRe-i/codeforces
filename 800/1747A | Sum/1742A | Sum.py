e0ae9d8805f6bdae33f98b5e39965091a2794d97
Leandro Bataglia <leandro.bataglia@gmail.com>
2023-12-02 13:42:22 -0300

    first commit

---
+++ 800/1747A | Sum/1742A | Sum.py
@@ -0,0 +1,13 @@
+n = int(input())
+for i in range(n):
+    values = [int(x) for x in input().split(" ")]
+    if (sum(values) ==  max(values)*2):
+        print("YES")
+    else:
+        print("NO")
+
+    
+
+
+
+
